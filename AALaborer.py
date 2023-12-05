670ce2185204c46ed2395c09c58f13bc32de3a47
Knullack <63751877+Knullack@users.noreply.github.com>
2023-12-05 01:47:19 -0500

    Improved file reading logic & optimization

--- AALaborer.py
+++ AALaborer.py
@@ -31,6 +31,101 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 excel_file_path = "C:/Users/adn51/Downloads/StaffingBoard_P1.xlsm"
 
+nrow = { #sheet name > path > role > number of rows down
+    'PS_ICQA_Learning': {
+        'ICQA': {
+            'directed counts': 10,
+            'pallet racking' : 10,
+            'simple bin counts' : 10,
+            'andons' : 10
+        },
+        'inbound': {
+            'IBJP': 5,
+            'Stow Kiosk': 5,
+            'Damages': 5,
+            'IOL': 5,
+            'Pallets': 5
+        },
+        'outbound':{
+            'OBJP': 5,
+            'runner': 5,
+            'UIS': 5,
+            'CPT Chase': 5,
+            'Pick Skips': 5
+        },
+        'learning':{
+            'general' : 13,
+            'pit': 13
+        }
+    },
+    'PIT': {
+        'stow': 99,
+        'pick': 99,
+        'OB forklift': 6,
+        'IB forklift': 6,
+        'IB tugger': 6,
+        'OB tugger': 6,
+        'tugger bus route': 6,
+        'center rider': 4,
+        'turret': 5,
+        'clamp': 5
+    },
+    'DOCK':{
+        'UIS': 6,
+        'Tote Palletize': 3,
+        'Tote Palletize WS': 2,
+        'Dock Auditor': 3,
+        'Palletize': 30,
+        'Palletize WS': 5,
+        'OB Line Load': 15,
+        'OB Line Load WS': 4,
+        'QA Scanner': 8,
+        'Grey Totes WS': 1
+    }
+}
+
+laborPath = { #range-name, path, process, labor-Code
+    # PS_ICQA_Learning
+    'counts' : ['directedCounts_start','ICQA','directed counts','ICQAPS'],
+    'pallet rack audits': ['palletRacking_start', 'ICQA', 'pallet racking','ICQAQA'],
+    'SBC': ['SimpleBinCount_start','ICQA','simple bin counts', 'ICQAQA'],
+    'andons':  ['andon_start','ICQA','andons','ICQAQA'],
+    'IBJP' : ['IBJackpot_start','inbound','IBJP','IBPS'],
+    'stowK' : ['stowKiosk_start','inbound','Stow Kiosk','IBPS'],
+    'dmg': ['damage_start','inbound','Damages','IBPS'],
+    'iol': ['IOL_start','inbound','IOL','IBPS'],
+    'palletPS': ['palletPS_start', 'inbound', 'Pallets','IBPS'],
+    'OBJP': ['OBJackpot_start','outbound','OBJP','PSTOPS'],
+    'OBrunner':['OBRunner_start','outbound','runner','PSTOPS'],
+    'UISPS': ['UISPS_start','outbound','UIS','PSTOPS'],
+    'CPT': ['CPTChase_start','outbound','CPT Chase','PSTOPS'],
+    'skips': ['pickSkips_start','outbound','Pick Skips','PSTOPS'],
+    'LearningGeneral': ['learningGeneral_start','learning','general','FCSCH'],
+    'LearningPIT': ['learningPIT_start','learning','pit','PITCLASS'],
+    # PIT
+    'stow': ['stow_start','PIT','stow',''], # stowers don't get labor tracked
+    'OBForklift': ['OBForklift_start','PIT','OB forklift',''],                                  # needs labor code
+    'IBForklift': ['IBForklift_start','PIT','IB forklift',''],                                  # needs labor code
+    'IBTug': ['IBTugger_start','PIT','IB tugger',''],                                           # needs labor code
+    'OBTug': ['OBTugger_start','PIT','OB tugger',''],                                           # needs labor code
+    'BusTug': ['tuggerBusRoute_start','PIT','tugger bus route',''],                             # needs labor code
+    'CR': ['centerRider_start','PIT','center rider',''],                                        # needs labor code
+    'Turret': ['turret_start','PIT','turret',''], #pallet stower, not labor tracked
+    'clamp': ['clampTruck_start','PIT','clamp',''],                                             # needs labor code
+    'pick': ['pick_start','PIT','pick',''], # pickers don't get labor tracked
+    # DOCK
+    'UIS': ['UISDock_start', 'DOCK','UIS',''],                                                  # needs labor code
+    'totePalletize': ['totePalletize_start','DOCK','Tote Palletize',''],                        # needs labor code
+    'totePalletizeWS': ['totePalletizeWS_start','DOCK','Tote Palletize WS',''],                 # needs labor code
+    'dockAudit': ['dockAuditor_start','DOCK','Dock Auditor',''],                                # needs labor code
+    'palletize': ['palletize_start','DOCK','Palletize',''],                                     # needs labor code
+    'palletizeWS': ['palletizeWS_start','DOCK','Palletize WS',''],                              # needs labor code
+    'OBLineload': ['OBLineLoad_start','DOCK','OB Line Load',''],                                # needs labor code
+    'OBLineLoadWS': ['OBLineLoadWS_start','DOCK','OB Line Load WS',''],                         # needs labor code
+    'QAScanner': ['QAScanner_start','DOCK','QA Scanner',''],                                    # needs labor code
+    'grayTotesWS': ['grayTotes_start', 'DOCK','grayTotes_start', '']                            # needs labor code
+}
+
 def HELPER_typeAndClick(element, textToType):
     element.send_keys(textToType)
     element.send_keys(Keys.ENTER)
@@ -78,53 +173,31 @@ def getBadges():
     palletize_AAs = palletize_Dock()
     print(palletize_AAs)
     
-def named_range(range_start, range_end, excel_path=excel_file_path):
+def named_range(range_start, laborPath_label, excel_path=excel_file_path):
     srange = pd.ExcelFile(excel_path).book.defined_names[range_start].value # returns 'SHEET!$col$row:$col$row'
-    erange = pd.ExcelFile(excel_path).book.defined_names[range_end].value
     sheet_name = str(srange.split('!')[0])
     column = str(srange.split('$')[1])
     start_row = int(srange.split('$')[2].split(':')[0])
-    end_row = int(erange.split('$')[2].split(':')[0])
-    return {'sheet':sheet_name, 'column':column, 'srow':start_row, 'erow':end_row}
+    return {'sheet':sheet_name, 'column':column, 'srow':start_row, 'erow':nrow[sheet_name][laborPath[laborPath_label][1]][laborPath[laborPath_label][2]]}
 
 def palletize_Dock():
-    range = 'badges_OB_Line_Load'
-    sheet = named_range(range)['sheet']
-    column = named_range(range)['column']
-    srow = named_range(range)['srow']
-    erow = named_range(range)['erow']
-    dataFrameVariable = pd.read_excel(io=excel_file_path, header=None, sheet_name=sheet, usecols=column, skiprows=srow-1, nrows=erow)
-    badgeIDs = HELPER_convertToSingleLineStr(dataFrameVariable.values)
-    return badgeIDs
+    range_start = 'badges_OB_Line_Load'
+    return badges(range_start)
 
 def UIS_Dock():
-    range = 'badge_UIS_DOCK'
-    sheet = named_range(range)['sheet']
-    column = named_range(range)['column']
-    srow = named_range(range)['srow']
-    erow = named_range(range)['erow']
-    dataFrameVariable = pd.read_excel(io=excel_file_path, header=None, sheet_name=sheet, usecols=column, skiprows=srow-1, nrows=erow)
-    badgeIDs = HELPER_convertToSingleLineStr(dataFrameVariable.values)
-    return badgeIDs
+    range_start = 'badge_UIS_DOCK'
+    return badges(range_start)
 
-def directed_counts():
-    range_start = 'directedCounts_start'
-    range_end = 'directedCounts_end'
-    sheet = named_range(range_start, range_end)['sheet']
-    column = named_range(range_start, range_end)['column']
-    srow = named_range(range_start, range_end)['srow']
-    erow = named_range(range_start, range_end)['erow']
-    dataFrameVariable = pd.read_excel(io=excel_file_path, header=None, sheet_name=sheet, usecols=column, skiprows=srow-1, nrows=erow-srow+1)
-    badgeIDs = HELPER_convertToSingleLineStr(dataFrameVariable.values)
-    return badgeIDs
+def directed_counts(CALM):
+    return badges(laborPath['counts'][0])
 
-def quality_audits():
-    range = 'quality_audits'
-    sheet = named_range(range)['sheet']
-    column = named_range(range)['column']
-    srow = named_range(range)['srow']
-    erow = named_range(range)['erow']
-    dataFrameVariable = pd.read_excel(io=excel_file_path, header=None, sheet_name=sheet, usecols=column, skiprows=srow-1, nrows=erow)
+def palletRackingAudits():
+    range_start = 'palletRacking_start'
+    return badges(range_start)
+
+def badges(range):
+    info = named_range(range, 'counts')
+    dataFrameVariable = pd.read_excel(io=excel_file_path, header=None, sheet_name=info['sheet'], usecols=info['column'], skiprows=info['srow']-1, nrows=info['erow'])
     badgeIDs = HELPER_convertToSingleLineStr(dataFrameVariable.values)
     return badgeIDs
 
@@ -136,8 +209,4 @@ def HELPER_convertToSingleLineStr(input):
         if n != 'nan':
             badgeList += n + " "
     return badgeList
-
-
-# LT(palletizeDock(),'TOTOL')
-directed_counts()
 
